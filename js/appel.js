8b886b3d59297a182c6cb692cadf46c3b96eb50f
Natuu <natyouu@gmail.com>
2013-10-14 21:07:19 +0200

    Activate GPU

---
+++ js/appel.js
@@ -0,0 +1,17 @@
+$(function() {
+
+	// Cette fonction permet de changer la couleur d'un eleve au clic
+
+	$('.person').click( function() {
+
+		if (!$(this).hasClass('away'))
+		{
+			$(this).addClass('away');
+		}
+		else
+		{
+			$(this).removeClass('away');
+		}
+	});
+
+});
