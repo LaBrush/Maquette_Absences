e0ed9ab9c23c8e7077ea31e964a12327563bb13b
Natuu <natyouu@gmail.com>
2013-10-15 18:35:09 +0200

    Change li to div for form

--- js/appel.js
+++ js/appel.js
@@ -14,4 +14,17 @@ $(function() {
 		}
 	});
 
+
+	// Cette fonction permet de n'afficher que les élèves dont le nom est mentionné dans la barre de recherche
+	$('#search').keyup(function() {
+
+		var personContains = '.person-hidden:contains("' + $(this).val() + '")';
+
+		$('.person').addClass('person-hidden').removeClass('person');
+
+		$(personContains).removeClass('person-hidden').addClass('person');
+
+	});
+
+
 });
